fac307da0092d9e2a472aa3a4ce0d15bd9a5364e
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2021-03-30 14:10:24 +0530

    [deploy] updated database URI

--- config/deployment/__init__.py
+++ config/deployment/__init__.py
@@ -6,5 +6,5 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 JWT_SECRET = os.environ.get('JWT_SECRET')
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/task_manager'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 BCRYPT_SALT = bcrypt.gensalt()
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app = create_app('config.development')
+app = create_app('config.deployment')
 
 if __name__ == '__main__':
     app.run()
